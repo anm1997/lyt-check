562e3f33785b438aa1330213e1e69af7586c59e4
Alim Marediya <alimmarediya@Alims-Laptop.lan>
2024-06-14 17:48:14 -0500

    update contact.js form

--- contact-form.js
+++ contact-form.js
@@ -1,30 +1,26 @@
 document.getElementById('contactForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-  
-    var formData = new FormData(this);
-    var object = {};
-    formData.forEach((value, key) => { object[key] = value });
-    var json = JSON.stringify(object);
-  
-    fetch('https://hooks.zapier.com/hooks/catch/19161262/2oaxlqn/', { // replace 'your_webhook_url' with the actual webhook URL from Zapier
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: json
-    })
-    .then(response => {
-      if (response.ok) {
-        document.getElementById('formResponse').style.display = 'block';
-      } else {
-        alert('There was a problem with the request.');
-      }
-    })
-    .catch(error => {
-      alert('There was an error submitting the form.');
-    });
+  e.preventDefault();
+
+  var formData = new FormData(this);
+  var object = {};
+  formData.forEach((value, key) => { object[key] = value });
+  var json = JSON.stringify(object);
+
+  fetch('https://hooks.zapier.com/hooks/catch/19161262/2oaxlqn/', { // replace 'your_webhook_url' with the actual webhook URL from Zapier
+    method: 'POST', // Use POST method
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: json
+  })
+  .then(response => {
+    if (response.ok) {
+      document.getElementById('formResponse').style.display = 'block';
+    } else {
+      alert('There was a problem with the request.');
+    }
+  })
+  .catch(error => {
+    alert('There was an error submitting the form.');
   });
-
-
-
-  
+});
